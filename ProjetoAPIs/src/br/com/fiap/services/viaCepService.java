d6e61499669d26296ba8f8ac0c088772be6fa99f
L3to <lf2006@hotmail.com.br>
2024-09-23 20:38:38 -0300

    Terminado integração com a API ViaCep

---
+++ ProjetoAPIs/src/br/com/fiap/services/viaCepService.java
@@ -0,0 +1,15 @@
+package br.com.fiap.services;
+
+import br.com.fiap.model.Endereco;
+
+public class viaCepService {
+	
+	public Endereco getEndereco() {
+
+		Endereco endereco = null;
+		
+		return endereco;
+		
+	}
+
+}
